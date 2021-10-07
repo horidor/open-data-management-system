6132d226530e5d01c9a7f62862181ed6a8424164
Maxim <makson202007@gmail.com>
2021-10-07 09:52:18 +0300

    changed main to master in publish.sh so that it would work

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:horidor/open-data-management-system.git master:gh-pages
+git push -f https://github.com/horidor/open-data-management-system.git main:gh-pages
 
 cd -
